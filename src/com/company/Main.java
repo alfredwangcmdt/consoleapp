edb380311ad8724ec03d4321310c0267ba57b802
alfred wang <alfred.wang@cm-dt.com>
2017-09-13 09:00:18 +0800

    01

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.printf("hello world");
+        System.out.printf("i am fine" );
+    }
+}
